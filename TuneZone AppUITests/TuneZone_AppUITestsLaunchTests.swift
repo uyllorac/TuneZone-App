1fa10256b0a7fdea495bddb4c420a7216937aaff
uyllorac@gmail.com <uyllorac@gmail.com>
2022-06-14 13:26:36 -0400

    Initial Commit

---
+++ TuneZone AppUITests/TuneZone_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TuneZone_AppUITestsLaunchTests.swift
+//  TuneZone AppUITests
+//
+//  Created by Carol Lyu on 2022-06-14.
+//
+
+import XCTest
+
+class TuneZone_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
