0cb378440a8b587022ef1da7b241ed47046e3e6a
uyllorac@gmail.com <uyllorac@gmail.com>
2022-06-17 11:06:40 -0400

    playlist action

---
+++ TuneZone App/PlaylistViewController.swift
@@ -0,0 +1,41 @@
+//
+//  PlaylistViewController.swift
+//  TuneZone App
+//
+//  Created by Carol Lyu on 2022-06-17.
+//
+
+import UIKit
+
+class PlaylistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func crushPlaylist(_ sender: Any) { 
+    }
+    @IBAction func nostalgicPlaylist(_ sender: Any) {
+    }
+    @IBAction func sadPlaylist(_ sender: Any) {
+    }
+    @IBAction func calmPlaylist(_ sender: Any) {
+    }
+    @IBAction func angryPlaylist(_ sender: Any) {
+    }
+    @IBAction func happyPlaylist(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
