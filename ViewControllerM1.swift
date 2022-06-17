c7a9479f857b1f90bc771eb42e5e43336e885766
stellameazza <stella.meazza@gmail.com>
2022-06-17 11:37:33 +0200

    add labels, add buttons, add images, add segues

--- ViewControllerM1.swift
+++ ViewControllerM1.swift
@@ -16,6 +16,13 @@ class ViewControllerM1: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+   
+        
+   
+           
+
+    
+    
     }
     
     
